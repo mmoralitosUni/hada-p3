b1fd4f140c18c5f95c2f7bfa881272e1f46a784e
mmorales99 <mjmorales.mcv@gmail.com>
2021-03-26 21:35:56 +0100

    48789509T adds database

---
+++ usuWeb/App_Data/dbo.Usuarios.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Usuarios]
+(
+[id] INT IDENTITY (1, 1) NOT NULL,
+[nombre] NVARCHAR (32) NOT NULL,
+[nif] NVARCHAR (16) NOT NULL,
+[edad] INT NOT NULL,
+PRIMARY KEY CLUSTERED ([id] ASC),
+UNIQUE NONCLUSTERED ([nif] ASC)
+)
